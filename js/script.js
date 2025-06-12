53b13667ba1d6aa942adce7c4d152631778488c3
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-06-12 14:44:49 +0600

    Counter

--- js/script.js
+++ js/script.js
@@ -2,17 +2,62 @@
 let counter01= document.getElementById('counter01')
 let counter02= document.getElementById('counter02')
 let counter03= document.getElementById('counter03')
+let counter04= document.getElementById('counter04')
 
-let count=0
+// Separate counts for each counter
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+let count4 = 0;
 
-function countjs (){
-count++
-counter01.innerText= count
-
+// __x
+function counterjs1() {
+  count1++;
+  counter01.innerText = count1;
+  if (count1 == 27) {
+    clearInterval(x);
+  }
 }
+let x = setInterval(() => {
+  counterjs1();
+}, 200);
 
+// _____y
+function counterjs2() {
+  count2++;
+  counter02.innerText = count2;
+  if (count2 == 11) {
+    clearInterval(y);
+  }
+}
+let y = setInterval(() => {
+  counterjs2();
+}, 300);
 
-// CounterUp
+// _____z
+function counterjs3() {
+  count3++;
+  counter03.innerText = count3;
+  if (count3 == 301) {
+    clearInterval(z);
+  }
+}
+let z = setInterval(() => {
+  counterjs3();
+}, 30);
+
+// _____xyz
+function counterjs4() {
+  count4++;
+  counter04.innerText = count4;
+  if (count4 == 34) {
+    clearInterval(a);
+  }
+}
+let a = setInterval(() => {
+  counterjs4();
+}, 80);
+//  CounterUp
 
 
 //Back to Top
